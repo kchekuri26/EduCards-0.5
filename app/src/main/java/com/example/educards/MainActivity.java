66b85c49d3ea86f9544726b34f06b6deb469b67d
kchekuri26 <57776360+kchekuri26@users.noreply.github.com>
2020-02-15 12:06:55 -0800

    All Done

--- app/src/main/java/com/example/educards/MainActivity.java
+++ app/src/main/java/com/example/educards/MainActivity.java
@@ -6,11 +6,14 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Shows the answer on the back of the card when clicked on the question.
         findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -19,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Shows the question on the card when clicked on the answer.
         findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -26,5 +30,70 @@ public class MainActivity extends AppCompatActivity {
                 findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
             }
         });
+
+        findViewById(R.id.showOptions).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.showOptions).setVisibility(View.INVISIBLE);
+                findViewById(R.id.hideOptions).setVisibility(View.VISIBLE);
+                findViewById(R.id.option_one).setVisibility(View.VISIBLE);
+                findViewById(R.id.option_two).setVisibility(View.VISIBLE);
+                findViewById(R.id.option_three).setVisibility(View.VISIBLE);
+            }
+        });
+
+        findViewById(R.id.option_one).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.option_one).setBackgroundColor(getResources().getColor(R.color.colorAccent, null));
+                findViewById(R.id.option_three).setBackgroundColor(getResources().getColor(R.color.colorPrimary, null));
+            }
+        });
+
+        findViewById(R.id.option_two).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.option_two).setBackgroundColor(getResources().getColor(R.color.colorAccent, null));
+                findViewById(R.id.option_three).setBackgroundColor(getResources().getColor(R.color.colorPrimary, null));
+            }
+        });
+
+        findViewById(R.id.option_three).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.option_three).setBackgroundColor(getResources().getColor(R.color.colorPrimary, null));
+            }
+        });
+
+        findViewById(R.id.hideOptions).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.showOptions).setVisibility(View.VISIBLE);
+                findViewById(R.id.hideOptions).setVisibility(View.INVISIBLE);
+                findViewById(R.id.option_one).setVisibility(View.INVISIBLE);
+                findViewById(R.id.option_two).setVisibility(View.INVISIBLE);
+                findViewById(R.id.option_three).setVisibility(View.INVISIBLE);
+            }
+        });
+
+
+        findViewById(R.id.background).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+                findViewById(R.id.option_one).setBackground(getDrawable(R.drawable.option_background));
+                findViewById(R.id.option_two).setBackground(getDrawable(R.drawable.option_background));
+                findViewById(R.id.option_three).setBackground(getDrawable(R.drawable.option_background));
+
+            }
+        });
+
+
+
+
+
+
+
     }
 }
